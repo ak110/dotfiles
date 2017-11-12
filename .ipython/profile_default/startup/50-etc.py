38f295149716b7d70a2952d9220067537c8c88d8
ak11 <mark@aur.ll.to>
2017-11-12 16:11:38 +0900

    .

---
+++ .ipython/profile_default/startup/50-etc.py
@@ -0,0 +1,49 @@
+import functools
+import hashlib
+import json
+import math
+import multiprocessing as mp
+import os
+import pathlib
+import pickle
+import shutil
+import subprocess
+import sys
+import time
+import traceback
+import xml.etree.ElementTree as ET
+
+import numpy as np
+import pandas as pd
+import sklearn
+import sklearn.externals.joblib
+import sklearn.utils
+
+df = pd.DataFrame()
+df['a'] = [1, 2, 3, np.nan]
+df['b'] = [4, 5, 6, np.nan]
+df['c'] = [7, 8, np.nan, np.nan]
+
+a1 = np.array([.75, .1, .15])
+b1 = np.array([2, 3, 4])
+
+a2 = np.array([[.75, .1, .15], [2, 3, 4]])
+b2 = np.array([[3, 4, 5], [4, 5, 6]])
+
+a3 = np.array([[[.75, .1, .15], [2, 3, 4]], [[3, 4, 5], [4, 5, 6]]])
+b3 = np.array([[[2, 3, 4], [4, 5, 6]], [[3, 4, 5], [1, 2, 3]]])
+
+work_dir = pathlib.Path(os.environ['USERPROFILE']).joinpath('Desktop')
+if not p.exists():
+    work_dir = pathlib.Path(os.environ['HOE'])
+if p.exists():
+    os.chdir(str(work_dir))
+
+def softmax(x):
+    # np.exp(x) / np.sum(np.exp(x))
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
