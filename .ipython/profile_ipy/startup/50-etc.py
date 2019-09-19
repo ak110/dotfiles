0491d86a9e8a003ef71de917f86ed2ac0a78e7d3
aki <mark@aur.ll.to>
2019-09-19 09:36:02 +0900

    ipy

--- .ipython/profile_ipy/startup/50-etc.py
+++ .ipython/profile_ipy/startup/50-etc.py
@@ -1,3 +1,4 @@
+"""ipythonでコード片を軽く動作確認したりするため用の準備コード。"""
 import functools
 import hashlib
 import json
@@ -6,6 +7,7 @@ import multiprocessing as mp
 import os
 import pathlib
 import pickle
+import random
 import shutil
 import subprocess
 import sys
@@ -22,6 +24,54 @@ import sklearn.metrics
 import sklearn.utils
 import joblib
 
+try:
+    import tensorflow as tf
+except ImportError:
+    print("skip: import tensorflow as tf")
+
+try:
+    from bashplotlib.scatterplot import plot_scatter
+    from bashplotlib.histogram import plot_hist
+except ImportError:
+    print("ImportError: bashplotlib")
+
+pytoolkit_home = os.environ.get("PYTOOLKIT_HOME")
+if pytoolkit_home is not None and pathlib.Path(pytoolkit_home).exists():
+    sys.path.append(pytoolkit_home)
+    print(f"PYTOOLKIT_HOME: {pytoolkit_home}")
+try:
+    import pytoolkit as tk
+except ImportError:
+    print("skip: import pytoolkit as tk")
+del pytoolkit_home
+
+def softmax(x):
+    # np.exp(x) / np.sum(np.exp(x))
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def logit(x):
+    if x == 1.0:
+        return np.inf
+    if x == 0.0:
+        return -np.inf
+    assert 0.0 < x < 1.0, f"Invalid value: {x}"
+    return np.log(x / (1.0 - x))
+
+if "USERPROFILE" in os.environ:
+    work_dir = pathlib.Path(os.environ["USERPROFILE"]) / "Desktop"
+elif "HOME" in os.environ:
+    work_dir = pathlib.Path(os.environ["HOME"])
+else:
+    work_dir = None
+if work_dir is not None:
+    os.chdir(str(work_dir))
+    print(f"chdir: {work_dir}")
+
 df = pd.DataFrame()
 df["a"] = [1, 2, 3, np.nan]
 df["b"] = [4, 5, 6, np.nan]
@@ -37,32 +87,6 @@ b2 = np.array([[3, 4, 5], [4, 5, 6]])
 a3 = np.array([[[.75, .1, .15], [2, 3, 4]], [[3, 4, 5], [4, 5, 6]]])
 b3 = np.array([[[2, 3, 4], [4, 5, 6]], [[3, 4, 5], [1, 2, 3]]])
 
-if "USERPROFILE" in os.environ:
-    work_dir = pathlib.Path(os.environ["USERPROFILE"]) / "Desktop"
-elif "HOME" in os.environ:
-    work_dir = pathlib.Path(os.environ["HOME"])
-else:
-    work_dir = None
-if work_dir is not None:
-    os.chdir(str(work_dir))
-    print(f"chdir: {work_dir}")
-
-def softmax(x):
-    # np.exp(x) / np.sum(np.exp(x))
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def logit(x):
-    if x == 1.0:
-        return np.inf
-    if x == 0.0:
-        return -np.inf
-    assert 0.0 < x < 1.0, f"Invalid value: {x}"
-    return np.log(x / (1.0 - x))
-
 d = {
     "a": 1,
     "b": 2,
@@ -70,14 +94,4 @@ d = {
     np.nan: "4",
 }
 
-
-pytoolkit_home = os.environ.get("PYTOOLKIT_HOME")
-if pytoolkit_home is not None and pathlib.Path(pytoolkit_home).exists():
-    sys.path.append(pytoolkit_home)
-    print(f"PYTOOLKIT_HOME: {pytoolkit_home}")
-try:
-    import pytoolkit as tk
-except ImportError:
-    print("skip: import pytoolkit as tk")
-
-import tensorflow as tf
+get_ipython().run_line_magic('load_ext', 'autoreload')
