e74c1a6b66378ca81d5632f1585f4035c9dbed7d
aki <mark@aur.ll.to>
2019-09-19 09:36:02 +0900

    ipy

--- deploy.bat
+++ deploy.bat
@@ -8,6 +8,7 @@ if "%XDG_CONFIG_HOME%"=="" (
 if exist "%XDG_CONFIG_HOME%" rmdir "%XDG_CONFIG_HOME%"
 mklink /D /J "%XDG_CONFIG_HOME%" "%~dp0.config"
 
+if exist "%USERPROFILE%\.ipython" rmdir "%USERPROFILE%\.ipython"
 mklink /D /J "%USERPROFILE%\.ipython" "%~dp0.ipython"
 
 pause
